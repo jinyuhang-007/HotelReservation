c10f43496ae155e5bc20ae4190fe36ec510d330c
jinyuhang-007 <53509988+jinyuhang-007@users.noreply.github.com>
2021-10-21 13:38:12 +0800

    add HotelApplication and AdminMenu

--- src/api/MainMenu.java
+++ src/api/MainMenu.java
@@ -1,5 +1,11 @@
 package api;
 
 public class MainMenu {
-
+    public void printMainMenu(){
+        System.out.println("1. Find and reserve a room");
+        System.out.println("2. See my reservations");
+        System.out.println("3. Create an account");
+        System.out.println("4. Admin");
+        System.out.println("5. Exit");
+    }
 }
